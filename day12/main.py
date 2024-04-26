77bd35974a6b6e57bae6c769c17448bbcd4deed2
Nkululeko <nbshongwes@gmail.com>
2024-04-27 00:42:01 +0200

    deleting spaces

--- day12/main.py
+++ day12/main.py
@@ -1,19 +1,6 @@
 from art import logo
 import random 
 
-
-""""
-A game of guessing any between 1 - 100, The user will get either 5 or 10 attempts depending on the level their chose. 
-The user will be informed if the number is lower/higher than the correct number. 
-"""
-
-
-#generate a random number between 1 to 100. 
-#choose the game level - input if the game is easy then set the chances/attempts to 10 and 5 - hard 
-#choose a number between 1 - 100
-#conditions for each number chosen, inform the user if the number is too low or too high
-
-
 def set_level():
     game_level = str(input("Choose a difficulty. Type 'easy' or 'hard': "))
     if game_level.lower() == "easy":
@@ -23,13 +10,11 @@ def set_level():
     return attempts
 
 def main():
+
     print(logo)
 
     attempts = set_level()
     correct_number = random.randint(0,100)
-
-    
-
     game_over = False
     
     while not game_over:
@@ -44,16 +29,13 @@ def main():
         elif guess == correct_number:
             game_over = True
             print(f"You got it. The correct answer was {guess}.")
-
-        attempts -= 1
-        
+   
         if attempts == 0:
             game_over = True
             print("You've ran out of guesses, you lose")
         elif not game_over:
+            attempts -= 1
             print("Guess again")
-
-
 
 if __name__ == "__main__":
     main()
